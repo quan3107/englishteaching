22fdbb44073bee4fe073e26c45c729e91dd9d40a
Quan <dinhquan97@gmail.com>
2025-05-10 10:55:35 +0700

    new file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import { Container, Typography, Paper } from '@mui/material'
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <Navbar />
+      <Container maxWidth={false} disableGutters sx={{ mt: 4, mb: 4 }}>
+        <Paper elevation={3} sx={{ p: 3, mt: 3, borderRadius: 0 }}>
+          <Typography variant="h3" component="h1" gutterBottom>
+            Welcome to Nhi Chau
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Explore our courses and resources to enhance your learning journey!
+          </Typography>
+        </Paper>
+      </Container>
+    </div>
+  )
+}
+
+export default App
