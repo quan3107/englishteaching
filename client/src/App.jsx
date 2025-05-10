8723fb7481d8ace31694f8210fb7dae974d76bee
Quan <dinhquan97@gmail.com>
2025-05-10 16:01:19 +0700

    add course display for ielts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,11 @@ function App() {
   return (
     <div className="app-container">
       <Navbar />
-      <Container maxWidth={false} disableGutters sx={{   flexGrow: 1, mt: 4, mb: 4 }}>
+      <Container
+        maxWidth={false}
+        disableGutters
+        sx={{ flexGrow: 1, mt: 4, mb: 4 }}
+      >
         <Paper elevation={3} sx={{ p: 3, mt: 3, borderRadius: 0 }}>
           <Typography variant="h3" component="h1" gutterBottom>
             Welcome to Nhi Chau English
@@ -17,34 +21,93 @@ function App() {
           <Typography variant="body1" paragraph>
             Explore our courses and resources to enhance your learning journey!
           </Typography>
-
         </Paper>
-
       </Container>
-      <Container
-        maxWidth={false}
-        disableGutters
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: 3,
-          justifyContent: "center",
-          mt: 4,
-          mb: 4,
-          px: 3
-        }}
-      >
-        <Course title="6th Grade" description="English Fundamentals for 6th Graders" />
-        <Course title="7th Grade" description="English Fundamentals for 7th Graders" />
-        <Course title="8th Grade" description="English Fundamentals for 8th Graders" />
-        <Course title="9th Grade" description="English Fundamentals for 9th Graders" />
-        <Course title="10th Grade" description="English Fundamentals for 10th Graders" />
-        <Course title="11th Grade" description="English Fundamentals for 11th Graders" />
-      </Container>
+      <Paper elevation={3} sx={{ p: 3, mt: 3, borderRadius: 0 }}>
+        
+          <Typography variant="h3" component="h1" gutterBottom>
+            Secondary School Courses
+          </Typography>
+        
+        <Container
+          maxWidth={false}
+          disableGutters
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 3,
+            justifyContent: "center",
+            mt: 4,
+            mb: 4,
+            px: 3,
+          }}
+        >
+          <Course
+            title="6th Grade"
+            description="English Fundamentals for 6th Graders"
+          />
+          <Course
+            title="7th Grade"
+            description="English Fundamentals for 7th Graders"
+          />
+          <Course
+            title="8th Grade"
+            description="English Fundamentals for 8th Graders"
+          />
+          <Course
+            title="9th Grade"
+            description="English Fundamentals for 9th Graders"
+          />
+          <Course
+            title="10th Grade"
+            description="English Fundamentals for 10th Graders"
+          />
+          
+        </Container>
+      </Paper>
+      <Paper elevation={3} sx={{ p: 3, mt: 3, borderRadius: 0 }}>
+        
+          <Typography variant="h3" component="h1" gutterBottom>
+            IELTS Courses
+          </Typography>
+        
+        <Container
+          maxWidth={false}
+          disableGutters
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: 3,
+            justifyContent: "center",
+            mt: 4,
+            mb: 4,
+            px: 3,
+          }}
+        >
+          <Course
+            title="IELTS Speaking"
+            description="IELTS Speaking Fundamentals"
+          />
+          <Course
+            title="IELTS Reading"
+            description="IELTS Reading Fundamentals"
+          />
+          <Course
+            title="IELTS Listening"
+            description="IELTS Listening Fundamentals"
+          />
+          <Course
+            title="IELTS Writing"
+            description="IELTS Writing Fundamentals"
+          />
+          
+          
+        </Container>
+      </Paper>
 
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App
