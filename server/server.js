ada199094c34981fff1b8ca05f0d937fd3067ab7
Quan <dinhquan97@gmail.com>
2025-05-12 16:15:02 +0700

    add signup data to sql + persist user login info

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 const corsOptions = {
     origin: "http://localhost:5173",
     credentials: true,
@@ -20,7 +21,7 @@ const corsOptions = {
 const db = new pg.Client({
     user: "postgres",
     host: "localhost",
-    database: "secrets",
+    database: "NCEstudents",
     password: "07Overwatch.",
     port: 5432
 })
@@ -32,9 +33,12 @@ app.use(cors(corsOptions));
 app.use(session({
     secret: "something",
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
-        maxAge: 600000
+        maxAge: 600000,
+        
+        
+        sameSite: "lax"
     }
 }));
 
@@ -44,14 +48,65 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+
+
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.get("/api/check-auth", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.json({isAuthenticated: true, user: req.user});
+    } else {
+        res.json({isAuthenticated: false})
+    }
+})
+
+app.get("/api/logout", (req, res) => {
+    req.logout((err) => {
+        if (err) { 
+            return res.status(500).json({ error: err.toString() }); 
+        }
+        res.json({ success: true, message: "Logged out successfully" });
+    })
+})
+
 app.post("/api/login", passport.authenticate("local"), (req, res) => {
     console.log(req.user);
     res.json({user:req.user})
 })
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
+app.post("/api/signup", async (req, res) => {
+    console.log(req.body);
+    const email = req.body.username;
+    const password = req.body.password;
+    try {
+        const checkResult = await db.query("SELECT * FROM students WHERE email = $1", [email,])
+        if (checkResult.rows.length > 0) {
+            console.log("Email already exists");
+            res.json({message: "Email already exists"});
+            // res.sendStatus(200).json({message: "Email already exists"});
+        } else {
+            bcrypt.hash(password, saltRounds, async (err, hash) => {
+                if (err) {
+                    console.log("Error hasing password: ", err);
+                } else {
+                    const result = await db.query(
+                        "INSERT INTO students (email, password) VALUES ($1, $2) RETURNING *", [email, hash]
+                    );
+                    res.json({message: "Success"});
+                    // res.sendStatus(201);
+                }
+            } )
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+    
+})
 
 app.listen(port, () => {
     console.log("Server started on port " + port);
@@ -65,7 +120,7 @@ passport.use("local",
         //     return cb(null, false);
         // }
         try {
-            const result = await db.query("SELECT * FROM users WHERE email = $1", [username,]);
+            const result = await db.query("SELECT * FROM students WHERE email = $1", [username,]);
             if (result.rows.length > 0) {
                 const user = result.rows[0];
                 const storedHasedPassword = user.password;
