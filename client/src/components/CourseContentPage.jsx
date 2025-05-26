b52ebde359df06bc12de347b6a9026f4cd4c9b7c
Quan <dinhquan97@gmail.com>
2025-05-26 13:42:16 +0700

    parent to render course content component based on which course is picked

---
+++ client/src/components/CourseContentPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+import CourseContent from "./CourseContent";
+import { useAuth } from "../Routes/Auth";
+import axios from "axios";
+
+function CourseContentPage() {
+    const {courseId, lessonId} = useParams();
+    const {user} = useAuth();
+    return (<CourseContent courseId={courseId} lessonId={lessonId}/>)
+}
+
+export default CourseContentPage;
